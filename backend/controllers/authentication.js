ed48df9bb6453124d0d73ae77e642ae7589a4d33
Scott Fanetti <scott.fanetti@gmail.com>
2022-12-18 12:57:28 -0500

    Adding changes that added login and digest

---
+++ backend/controllers/authentication.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const db = require('../models');
+const bcrypt = require('bcrypt');
+
+const { User } = db;
+
+router.post('/', async (req, res) => {
+    let user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: 'Ur user cannot be founded'
+        });
+    } else {
+        res.json({ user });
+    }
+})
+
+module.exports = router;
